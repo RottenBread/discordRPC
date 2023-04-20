966f94f867ba5f2286a4a291cd5dcec3695cbddc
Junhyeok Moon <mig070@naver.com>
2023-04-20 23:37:30 +0900

    Update RPC.py

--- RPC.py
+++ RPC.py
@@ -9,8 +9,8 @@ RPC.connect()
 while True:
     RPC.update(
         large_image = "3296160",
-        large_text = "이것은 책이여",
-        state = "현대사회와정신건강 (BDU)",
+        large_text = "This is a book",
+        state = "My Study",
         start = start
     )
     time.sleep(60)
